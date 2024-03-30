6b542b2b691c86c2e8024bcda232c97a645cb448
marcus-wrd <123615006+marcus-wrd@users.noreply.github.com>
2024-03-30 14:30:04 +0800

    Update mapdata.ts

--- data/mapdata.ts
+++ data/mapdata.ts
@@ -6,61 +6,7 @@ export default {
       fx: -13.916222252976013,
       fy: -659.1641376795345
     },
-    {
-      id: "df788f47-b1d8-41a7-b529-6c24899f4c9f",
-      text: "health",
-      fx: 154.3247731601375,
-      fy: -429.73700786748157
-    },
-    {
-      id: "9066b5e5-5021-4e65-bb8e-9a1ff702956c",
-      text: "eat helathy food",
-      fx: 455.7839253819375,
-      fy: -183.5539283546699
-    },
-    {
-      id: "4b6bd0db-f2e9-4ab8-a966-aed4413d3da1",
-      text: "Physical Fitness",
-      fx: -98.5231997717085,
-      fy: -60.07462866512333
-    },
-    {
-      id: "9b53fa57-dbcd-4ac3-bca5-637ee9eae957",
-      text: "How to burn Calory?",
-    },
-    {
-      id: "16bb6b97-ab93-4536-ad68-50b95c7d4661",
-      text: "work-life balance"
-    },
-    {
-      id: "e5ab6e94-42e6-498c-b8c0-da34b4cc690b",
-      text: "Calory burning food"
-    }
   ],
   connections: [
-    {
-      source: "ab60a5d0-611e-49aa-9278-5b9b1a862cdd",
-      target: "4b6bd0db-f2e9-4ab8-a966-aed4413d3da1"
-    },
-    {
-      source: "df788f47-b1d8-41a7-b529-6c24899f4c9f",
-      target: "ab60a5d0-611e-49aa-9278-5b9b1a862cdd"
-    },
-    {
-      source: "4b6bd0db-f2e9-4ab8-a966-aed4413d3da1",
-      target: "16bb6b97-ab93-4536-ad68-50b95c7d4661"
-    },
-    {
-      source: "4b6bd0db-f2e9-4ab8-a966-aed4413d3da1",
-      target: "9b53fa57-dbcd-4ac3-bca5-637ee9eae957"
-    },
-    {
-      source: "9b53fa57-dbcd-4ac3-bca5-637ee9eae957",
-      target: "e5ab6e94-42e6-498c-b8c0-da34b4cc690b"
-    },
-    {
-      source: "9066b5e5-5021-4e65-bb8e-9a1ff702956c",
-      target: "df788f47-b1d8-41a7-b529-6c24899f4c9f"
-    }
   ]
 };
